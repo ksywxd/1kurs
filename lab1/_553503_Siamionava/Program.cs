7cb5d68ba96bf5aed6b35153fed20d523bc58002
ksywxd <xenia5224@gmail.com>
2026-02-17 23:27:14 +0300

    first

---
+++ lab1/_553503_Siamionava/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Vvedite delimoe:");
+        double delimoe = double.Parse(Console.ReadLine());
+
+        Console.Write("Vvedite delitel:");
+        double delitel = double.Parse(Console.ReadLine());
+
+        double res = delimoe / delitel;
+
+        Console.WriteLine($"Result: {res}");
+    }
+}
