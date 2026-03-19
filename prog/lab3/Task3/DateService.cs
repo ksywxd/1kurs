21452cfd74154874806334ac819023b889ffa9ac
ksywxd <xenia5224@gmail.com>
2026-03-19 10:46:26 +0300

    t3

---
+++ prog/lab3/Task3/DateService.cs
@@ -0,0 +1,20 @@
+﻿using System;
+namespace Task3
+{
+    internal class DateService
+    {
+        public string GetDay(string date)
+        {
+            DateTime dt = DateTime.Parse(date);
+            return dt.DayOfWeek.ToString();
+        }
+
+        public int GetDaysSpan(int day, int month, int year)
+        {
+            DateTime today = DateTime.Today;
+            DateTime inputDate = new DateTime(year, month, day);
+
+            return Math.Abs((inputDate - today).Days);
+        }
+    }
+}
---
+++ prog/lab3/Task3/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+namespace Task3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DateService ds = new DateService();
+
+            string day = ds.GetDay("27.11.2007");
+            Console.WriteLine($"Day of the week: {day}");
+
+            int daysBetween = ds.GetDaysSpan(27, 11, 2007);
+            Console.WriteLine($"Days between today and 27.11.2007: {daysBetween}");
+        }
+    }
+}
