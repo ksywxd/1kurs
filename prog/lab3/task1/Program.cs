1daab9c4b05da18de85b6d6401ec493a80d5bcc3
ksywxd <xenia5224@gmail.com>
2026-03-19 09:13:37 +0300

    t1

---
+++ prog/lab3/task1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+namespace lab3
+{
+    class Program
+    {
+        static void Main()
+        {
+            Swap mySwap = new Swap();
+            int input = 0;
+
+            Console.Write("Enter a 3-digit num: ");
+
+            while (!int.TryParse(Console.ReadLine(), out input) 
+                   || Math.Abs(input) < 100 || Math.Abs(input) > 999)
+            {
+                Console.WriteLine("Invalid input. Try again.");
+            }
+
+            int res = mySwap.doSwap(input);
+            Console.Write(res);
+        }
+    }
+}
---
+++ prog/lab3/task1/Swap.cs
@@ -0,0 +1,15 @@
+﻿using System;
+namespace lab3
+{
+    public class Swap
+    {
+        public int doSwap(int num)
+        {
+            int n1 = num / 100;
+            int n2 = (num / 10) % 10;
+            int n3 = num % 10;
+
+            return n3 * 100 + n2 * 10 + n1;
+        }
+    }
+}
